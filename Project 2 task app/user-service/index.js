b747ac04644e568d1552f9cee9a7dd7762492be0
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-29 21:33:13 +0530

    commit

--- Project 2 task app/user-service/index.js
+++ Project 2 task app/user-service/index.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const mongoose = require("mongoose");
+const bodyParse = require("body-parser")
+
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
