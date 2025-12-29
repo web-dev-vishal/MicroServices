bb5f3e41b0ac7364baac3b007d991bd22179e7fd
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-29 21:34:24 +0530

    commit

--- Project 2 task app/user-service/index.js
+++ Project 2 task app/user-service/index.js
@@ -3,12 +3,14 @@ const mongoose = require("mongoose");
 const bodyParse = require("body-parser")
 
 const app = express();
-const port = 3000;
+const port = 3001;
+
+app.use(bodyParse.json())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:3000`)
+    console.log(`Example app listening at http://localhost:3001`)
 })
