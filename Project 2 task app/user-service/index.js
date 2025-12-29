f08925ff01c47808270a6e9f75ffa4cb30398fc1
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-29 22:28:31 +0530

    commit

--- Project 2 task app/user-service/index.js
+++ Project 2 task app/user-service/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mongoose = require("mongoose");
-const bodyParse = require("body-parser")
+const bodyParser = require("body-parser"); 
 
 const app = express();
 const port = 3001;
 
-app.use(bodyParse.json());
+app.use(bodyParser.json());
 
 mongoose.connect('mongodb://mongodb:27017/users', {
 }).then(() => console.log('Connection to MongoDB successful✅'))
@@ -14,14 +14,14 @@ mongoose.connect('mongodb://mongodb:27017/users', {
 const UserSchema = new mongoose.Schema({
     name: String,
     email: String
-})
+});
 
-const User = mongoose.model('User', UserSchema)
+const User = mongoose.model('User', UserSchema);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:3001`)
-})
+    console.log(`Example app listening at http://localhost:3001`);
+});
