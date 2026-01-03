3946d34d22d77b635fac8ec1dc62f29fcf8f3e7e
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-03 20:07:47 +0530

    Project_3

--- Project 3/auth_micro/server.js
+++ Project 3/auth_micro/server.js
@@ -1,6 +1,12 @@
 import express from "express";
+import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.json({ message: "Auth Micro service working" })
