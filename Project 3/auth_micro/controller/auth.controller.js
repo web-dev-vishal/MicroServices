2a9be4f28c591ee643c3ef80ce7ad006a412084b
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-03 23:18:00 +0530

    Project_3

--- Project 3/auth_micro/controller/auth.controller.js
+++ Project 3/auth_micro/controller/auth.controller.js
@@ -1,6 +1,8 @@
+import bcrypt from "bcrypt"
 class AuthContoller {
     static async register(req, res) {
         const payload = req.body;
+        const salt = async
 
         return res.json(payload);
     }
