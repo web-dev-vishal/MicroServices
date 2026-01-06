6ec794f5226acc58cda18c9a6cf75bf5b051a8f7
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-06 19:08:42 +0530

    commit

--- Project 1 code snippet/comments/index.js
+++ Project 1 code snippet/comments/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-const PORT = 4000;
+const PORT = 8001;
 
 app.listen(PORT,()=>{
     console.log(`Example app listening at http://localhost:{PORT}`)
