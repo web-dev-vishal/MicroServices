5e990fb22de84babc3fb3274225229cb6def80a0
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-06 19:33:53 +0530

    commit

--- Project 1 code snippet/snippet/index.js
+++ Project 1 code snippet/snippet/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 
+import snippetRouter from "./router/snippet.route.js"
 
 const app = express();
 const PORT = 8000;
 
-app.use("/api/v1/snippet", snippe)
+app.use("/api/v1/snippet", snippetRouter);
 
 app.listen(PORT,()=>{
     console.log(`Example app listening at http://localhost:8000`)
