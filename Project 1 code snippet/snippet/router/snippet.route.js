46ba62e9798704659f25fc0c75120dca89bee489
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-06 19:30:51 +0530

    commit

--- Project 1 code snippet/snippet/router/snippet.route.js
+++ Project 1 code snippet/snippet/router/snippet.route.js
@@ -1,7 +1,8 @@
 import express from "express";
+import { createSnippet } from "../controller/snippet.contoller.js";
 
 const router = express.Router();
 
-
+router.route("/").post(createSnippet)
 
 export default router;
